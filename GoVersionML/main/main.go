2a83c5580173494d793bbe7bc3507852c8f5a895
Rick Hanna <117633538+PieWhite@users.noreply.github.com>
2026-04-07 09:49:40 +0200

    Added benchmarking tool

---
+++ GoVersionML/main/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"goversion/config"
+	"goversion/engine"
+	"goversion/ingest"
+	"goversion/output"
+	"goversion/reporter"
+)
+
+func main() {
+	if err := run(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	start := time.Now()
+
+	appConfig := &config.AppConfig{}
+	if err := appConfig.ParseArgs(args); err != nil {
+		if err == flag.ErrHelp {
+			os.Exit(0)
+		}
+		return err
+	}
+
+
+	out, cleanup, err := output.Setup(appConfig.OutputFile)
+	if err != nil {
+		return fmt.Errorf("failed to setup output: %w", err)
+	}
+	defer cleanup()
+
+	detector, err := engine.NewDetector(appConfig.ModelPath)
+	if err != nil {
+		return fmt.Errorf("Failed loading xgboost model %w", err)
+	}
+	fmt.Fprintf(out, "Scanning %s with XGBoost...\n", appConfig.NetflowPath)
+
+	flowIngestor := ingest.NewIngestor()
+
+	err = flowIngestor.ProcessInput(appConfig.NetflowPath, detector.ProcessRecords)
+	if err != nil {
+		return fmt.Errorf("scanning input file: %w", err)
+	}
+
+	results := detector.CalculateResults()
+	reporter.PrintSummary(out, results, detector.TotalRecords, time.Since(start))
+
+	if appConfig.OutputFile != "" {
+		fmt.Printf("Results written to: %s\n", appConfig.OutputFile)
+	}
+
+
+	return nil
+}
